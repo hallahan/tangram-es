22437d806ab5f4f81429068726baee057d6aa14a
Nicholas Hallahan <nick@theoutpost.io>
2016-07-10 19:57:10 -0700

    styling OSM XML buildings

--- osx/src/main.cpp
+++ osx/src/main.cpp
@@ -10,7 +10,7 @@
 // Forward declaration
 void init_main_window(bool recreate);
 
-std::string sceneFile = "nick.yaml";
+std::string sceneFile = "osm-xml-demo.yaml";
 
 GLFWwindow* main_window = nullptr;
 int width = 800;
